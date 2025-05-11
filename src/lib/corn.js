4aa5b376ddbcfe56ead92877b9ab5ce2ed4a7bc1
Muhammad BIn Azeem <mbam095@gmail.com>
2025-05-11 19:28:39 +0500

    corn job dynamically added

--- src/lib/corn.js
+++ src/lib/corn.js
@@ -1,13 +1,26 @@
-import corn from 'corn';
+import { CronJob } from 'cron';
+import http from 'http';
 import https from 'https';
+import { URL } from 'url';
 
-const job = new corn.CornJob("*/14 * * * *", function() {
-    https
-        .get(process.env.API_URL, (res) => {
-            if (res.statusCode === 200) console.log("GET request send successfully");
-            else console.log("GET request failed", res.statusCode)
-        })
-        .on("error", (e) => console.log("Error while sending request", e));
+const job = new CronJob("*/14 * * * *", function () {
+    try {
+        const url = new URL(process.env.API_URL);
+        const lib = url.protocol === 'https:' ? https : http;
+
+        lib.get(url, (res) => {
+            if (res.statusCode === 200) {
+                console.log("GET request sent successfully");
+            } else {
+                console.log("GET request failed", res.statusCode);
+            }
+        }).on("error", (e) => {
+            console.log("Error while sending request", e);
+        });
+
+    } catch (err) {
+        console.error("Invalid URL or error in request setup:", err);
+    }
 });
 
 export default job;
