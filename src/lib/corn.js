0c66d929f4359ba389f543a4e58b21036ce77bde
Muhammad BIn Azeem <mbam095@gmail.com>
2025-06-30 16:22:15 +0500

    change cornjob from 15 mins to once in a day

--- src/lib/corn.js
+++ src/lib/corn.js
@@ -3,7 +3,7 @@ import http from 'http';
 import https from 'https';
 import { URL } from 'url';
 
-const job = new CronJob("*/14 * * * *", function () {
+const job = new CronJob("0 0 * * *", function () {
     try {
         const url = new URL(process.env.API_URL);
         const lib = url.protocol === 'https:' ? https : http;
